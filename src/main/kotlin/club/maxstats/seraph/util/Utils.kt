286898fe870fb6ca9be0ffbd91fce100e5c97f89
exejar <56853437+exejar@users.noreply.github.com>
2023-05-13 16:51:05 -0400

    Added a simple player-cache

--- src/main/kotlin/club/maxstats/seraph/util/Utils.kt
+++ src/main/kotlin/club/maxstats/seraph/util/Utils.kt
@@ -1,12 +1,17 @@
 package club.maxstats.seraph.util
 
 import club.maxstats.seraph.Main
+import getPlayerFromUUID
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import net.minecraft.client.Minecraft
+import player.Player
 import java.io.InputStream
+import java.util.*
+import kotlin.collections.HashMap
 
-val safeJson = Json { ignoreUnknownKeys = true }
+private val safeJson = Json { ignoreUnknownKeys = true }
+private val playerCache = HashMap<UUID, Player>()
 fun String.asResource() : InputStream = Main::class.java.classLoader.getResourceAsStream(this)
     ?: throw IllegalStateException("Failed to fetch resource $this")
 fun now() = System.currentTimeMillis()
@@ -28,4 +33,9 @@ fun calculateScaleFactor(mc: Minecraft): Int {
     if (isUnicode && scaleFactor % 2 != 0 && scaleFactor != 1) --scaleFactor
 
     return scaleFactor
+}
+suspend fun UUID.getPlayerData() : Player {
+    return playerCache.getOrPut(this) {
+        getPlayerFromUUID(this.toString(), "")
+    }
 }
