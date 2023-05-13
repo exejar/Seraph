a6ae704bf1dd23e3c5e4241c5f934283ce98630b
exejar <56853437+exejar@users.noreply.github.com>
2023-05-13 17:27:18 -0400

    Event subscription to populate player cache

--- src/main/kotlin/club/maxstats/seraph/Main.kt
+++ src/main/kotlin/club/maxstats/seraph/Main.kt
@@ -1,9 +1,21 @@
 package club.maxstats.seraph
 
+import club.maxstats.seraph.util.getPlayerData
+import club.maxstats.seraph.util.locrawInfo
 import club.maxstats.weave.loader.api.ModInitializer
+import club.maxstats.weave.loader.api.event.EventBus
+import club.maxstats.weave.loader.api.event.PlayerListEvent
+import club.maxstats.weave.loader.api.event.SubscribeEvent
 
 class Main : ModInitializer {
     override fun init() {
+        EventBus.subscribe(this)
+    }
 
+    @SubscribeEvent
+    suspend fun onListAdd(event: PlayerListEvent.Add) {
+        if (locrawInfo.inGame()) {
+            event.playerData.profile.id.getPlayerData()
+        }
     }
 }
