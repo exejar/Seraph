32ded9d03bca998515b3e9b6f031d861f0584a5a
exejar <56853437+exejar@users.noreply.github.com>
2023-05-13 14:35:05 -0400

    Added custom serializer for GameType enum

--- src/main/kotlin/club/maxstats/seraph/util/LocrawInfo.kt
+++ src/main/kotlin/club/maxstats/seraph/util/LocrawInfo.kt
@@ -1,21 +1,49 @@
 package club.maxstats.seraph.util
 
 import com.google.gson.annotations.SerializedName
-import kotlinx.serialization.Serializable
+import kotlinx.serialization.*
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
 
 @Serializable
 data class LocrawInfo(
-    @SerializedName("server") val server: String,
+    @SerializedName("server") val server: String = "",
 
     /* Present when you're in a lobby */
-    @SerializedName("gametype") val gameType: GameType,
-    @SerializedName("lobbyname") val lobbyName: String,
+    @SerializedName("gametype") val gameType: GameType = GameType.UNKNOWN,
+    @SerializedName("lobbyname") val lobbyName: String = "",
 
     /* Present when you're in a game */
-    @SerializedName("mode") val mode: String,
-    @SerializedName("map") val map: String
+    @SerializedName("mode") val mode: String = "",
+    @SerializedName("map") val map: String = ""
 )
+@OptIn(ExperimentalSerializationApi::class)
+@Serializer(forClass = GameType::class)
+object GameTypeSerializer : KSerializer<GameType> {
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("GameType", PrimitiveKind.STRING)
+    override fun deserialize(decoder: Decoder): GameType {
+        val gameTypeStr = decoder.decodeString()
+        return GameType.values().firstOrNull { it.gameType == gameTypeStr }
+            ?: GameType.UNKNOWN
+    }
+    override fun serialize(encoder: Encoder, value: GameType) {
+        encoder.encodeString(value.gameType)
+    }
+}
 
+val locrawJson = Json {
+    serializersModule = SerializersModule { contextual(GameType::class, GameTypeSerializer) }
+    ignoreUnknownKeys = true
+}
+fun deserializeLocraw(json: String) {
+    locrawInfo = locrawJson.decodeFromString<LocrawInfo>(json)
+}
 var locrawInfo: LocrawInfo = LocrawInfo("", GameType.UNKNOWN, "", "", "")
 
 /**
