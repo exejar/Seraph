8c499edea8959a8adcd4e55319e8874bfcdd719b
exejar <56853437+exejar@users.noreply.github.com>
2023-05-14 20:26:57 -0400

    Configured shaded jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
     kotlin("jvm") version "1.8.20"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.0"
+    kotlin("plugin.serialization") version "1.8.21"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     id("com.github.weave-mc.weave-gradle") version "823628f548"
     application
 }
@@ -14,17 +17,33 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+val runtime: Configuration by configurations.creating
+configurations.implementation.get().extendsFrom(runtime)
+runtime.isTransitive = false
 
-    implementation("com.github.exejar:HyKo:79365e92b3")
+dependencies {
+    runtime("com.github.exejar:HyKo:79365e92b3")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     compileOnly("com.github.weave-mc:weave-loader:c8c6186117")
     compileOnly("org.spongepowered:mixin:0.8.5")
 }
 
 minecraft.version("1.8.9")
 
-tasks.jar {
-    destinationDirectory.set(file("${System.getProperty("user.home")}/.lunarclient/mods"))
-    archiveBaseName.set("Seraph")
+application {
+    mainClass.set("club.maxstats.seraph.Main")
+}
+
+tasks {
+    withType<ShadowJar> {
+        configurations = listOf(runtime)
+        destinationDirectory.set(file("${System.getProperty("user.home")}/.lunarclient/mods"))
+    }
+    named<Jar>("jar") {
+        enabled = false
+    }
+    build {
+        dependsOn("shadowJar")
+    }
 }
