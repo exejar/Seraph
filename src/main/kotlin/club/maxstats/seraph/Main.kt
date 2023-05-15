baf6e66cb40880dbac3fb93c0872ed161df8f43e
exejar <56853437+exejar@users.noreply.github.com>
2023-05-14 20:28:17 -0400

    Basic coroutine handling (broken on Lunar)

--- src/main/kotlin/club/maxstats/seraph/Main.kt
+++ src/main/kotlin/club/maxstats/seraph/Main.kt
@@ -1,46 +1,43 @@
 package club.maxstats.seraph
 
-import club.maxstats.seraph.util.apiKey
-import club.maxstats.seraph.util.getPlayerData
-import club.maxstats.seraph.util.isOnHypixel
-import club.maxstats.seraph.util.locrawInfo
+import club.maxstats.seraph.render.FontManager
+import club.maxstats.seraph.render.Position
+import club.maxstats.seraph.render.none
+import club.maxstats.seraph.util.*
 import club.maxstats.weave.loader.api.ModInitializer
 import club.maxstats.weave.loader.api.event.ChatReceivedEvent
 import club.maxstats.weave.loader.api.event.EventBus
 import club.maxstats.weave.loader.api.event.PlayerListEvent
 import club.maxstats.weave.loader.api.event.SubscribeEvent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import net.minecraft.client.Minecraft
+import net.minecraft.util.ChatComponentText
 import java.io.File
 
+lateinit var fontManager: FontManager
 class Main : ModInitializer {
-    private val file = File("seraph_cache")
-
+    val coroutineScope = CoroutineScope(Dispatchers.Default)
     override fun init() {
+        fontManager = FontManager()
         EventBus.subscribe(this)
-        apiKey = Json.decodeFromString(ApiKey.serializer(), file.readText()).apiKey
+        EventBus.subscribe(ApiKey())
     }
 
     @SubscribeEvent
-    suspend fun onListAdd(event: PlayerListEvent.Add) {
+    fun onListAdd(event: PlayerListEvent.Add) {
+        println("List Add")
         if (locrawInfo.inGame()) {
-            event.playerData.profile.id.getPlayerData()
+            Minecraft.getMinecraft().thePlayer.addChatMessage(ChatComponentText("${event.playerData.profile.name} grabbing stats"))
+
+            coroutineScope.launch {
+                val playerData = event.playerData.profile.id.getPlayerData() ?: return@launch
+                none("${playerData.displayName}'s Stats", "${playerData.stats.bedwars.overall.finalKills} Final Kills", 4000L, Position.BOTTOM_RIGHT)
+            }
         }
     }
-
-    @SubscribeEvent
-    fun onChat(event: ChatReceivedEvent) {
-        if (event.message.unformattedText.startsWith("Your new API key is ") && isOnHypixel) {
-            val key = event.message.unformattedText.replace("Your new API key is ", "")
-            val json = Json.encodeToString(ApiKey.serializer(), ApiKey(key))
-            file.writeText(json)
-            apiKey = key
-        }
-    }
-
-    @Serializable
-    data class ApiKey(
-        @SerialName("api_key") val apiKey: String
-    )
 }
