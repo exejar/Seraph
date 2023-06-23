aa3154383f1581771a941f041c2132f4c375912a
exejar <56853437+exejar@users.noreply.github.com>
2023-06-23 16:28:26 -0400

    Updated to the newest version of Kolour

--- src/main/kotlin/club/maxstats/seraph/config/Config.kt
+++ src/main/kotlin/club/maxstats/seraph/config/Config.kt
@@ -6,73 +6,83 @@ import club.maxstats.kolour.util.Color
 import club.maxstats.seraph.render.renderPlayerModel
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.GuiScreen
-import net.minecraft.client.gui.inventory.GuiInventory
-import net.minecraft.client.renderer.EntityRenderer
-import net.minecraft.client.renderer.entity.RenderPlayer
-import net.minecraft.entity.player.EntityPlayer
-import net.minecraft.entity.player.InventoryPlayer
-import net.minecraft.util.ChatComponentText
-import org.lwjgl.input.Mouse
 
 val configCategory = component {
-    position = Position.RELATIVE
-    alignItems = ItemAlignment.MIDDLE
-    alignContent = ContentAlignment.MIDDLE
+    style {
+        position = Position.RELATIVE
+        alignItems = ItemAlignment.MIDDLE
+        alignContent = ContentAlignment.MIDDLE
 
-    width = 5.rem
-    height = 5.rem
-    margin = Sides(1.rem, 0.px, 1.rem, 0.px)
+        width = 5.rem
+        height = 5.rem
+        margin = Sides(1.rem, 0.px, 1.rem, 0.px)
 
-    backgroundColor = Color(60, 60, 60, 255)
-    borderRadius = Radius(10.px)
+        backgroundColor = Color(60, 60, 60, 255)
+        borderRadius = Radius(10.px)
+    }
 }
 
 val mainScreen = gui {
     id = "main-screen"
-    position = Position.FIXED
-    alignItems = ItemAlignment.MIDDLE
-    alignContent = ContentAlignment.MIDDLE
 
-    width = 100.vw
-    height = 100.vh
+    style {
+        position = Position.FIXED
+        alignItems = ItemAlignment.MIDDLE
+        alignContent = ContentAlignment.MIDDLE
 
-    backgroundColor = Color(25, 25, 25, 100)
-    blur = 18.px
+        width = 100.vw
+        height = 100.vh
+
+        backgroundColor = Color(25, 25, 25, 100)
+        blur = 18.px
+    }
 
     component {
         id = "content"
-        position = Position.ABSOLUTE
-        top = 100.vh
-        width = 100.vw
-        height = 1.rem
+
+        style {
+            position = Position.ABSOLUTE
+            top = 100.vh
+            width = 100.vw
+            height = 1.rem
+        }
 
         component {
             id = "left-panel"
-            position = Position.RELATIVE
-            direction = AlignDirection.COLUMN
-            alignItems = ItemAlignment.MIDDLE
-            alignContent = ContentAlignment.MIDDLE
 
-            width = 60.vw
-            height = 100.vh
-
-            component {
-                id = "stat-container"
+            style {
                 position = Position.RELATIVE
+                direction = AlignDirection.COLUMN
                 alignItems = ItemAlignment.MIDDLE
                 alignContent = ContentAlignment.MIDDLE
 
                 width = 60.vw
-                height = 50.vh
+                height = 100.vh
+            }
+
+            component {
+                id = "stat-container"
+
+                style {
+                    position = Position.RELATIVE
+                    alignItems = ItemAlignment.MIDDLE
+                    alignContent = ContentAlignment.MIDDLE
+
+                    width = 60.vw
+                    height = 50.vh
+                }
 
                 component {
                     id = "player-container"
-                    alignItems = ItemAlignment.MIDDLE
-                    alignContent = ContentAlignment.MIDDLE
-                    width = 5.rem
-                    height = 8.rem
-                    backgroundColor = Color(45, 45, 45, 255)
-                    borderRadius = Radius(6.px)
+
+                    style {
+                        alignItems = ItemAlignment.MIDDLE
+                        alignContent = ContentAlignment.MIDDLE
+                        width = 5.rem
+                        height = 8.rem
+                        backgroundColor = Color(45, 45, 45, 255)
+                        borderRadius = Radius(6.px)
+                    }
 
                     onRender {
                         // render player model using minecraft's method
@@ -91,35 +101,50 @@ val mainScreen = gui {
         }
         component {
             id = "right-panel"
-            position = Position.RELATIVE
-            direction = AlignDirection.COLUMN
-            alignItems = ItemAlignment.MIDDLE
-            alignContent = ContentAlignment.MIDDLE
-            height = 100.vh
-            width = 40.vw
+
+            style {
+                position = Position.RELATIVE
+                direction = AlignDirection.COLUMN
+                alignItems = ItemAlignment.MIDDLE
+                alignContent = ContentAlignment.MIDDLE
+                height = 100.vh
+                width = 40.vw
+            }
 
             header {
-                fontStyle = FontStyle.BOLD
-                text = "Seraph Settings"
-                margin.bottom = 4.rem
-            }
-            (+configCategory).modify {
-                bottom = 2.rem
-
-                paragraph {
-                    text = "General"
+                style {
+                    fontStyle = FontStyle.BOLD
+                    text = "Seraph Settings"
+                    margin.bottom = 4.rem
                 }
             }
-            (+configCategory).modify {
-                paragraph {
-                    text = "Display"
+            +configCategory + {
+                style.modify {
+                    bottom = 2.rem
                 }
-            }
-            (+configCategory).modify {
-                top = 2.rem
 
                 paragraph {
-                    text = "Other"
+                    style {
+                        text = "General"
+                    }
+                }
+            }
+            +configCategory + {
+                paragraph {
+                    style {
+                        text = "Display"
+                    }
+                }
+            }
+            +configCategory + {
+                style.modify {
+                    top = 2.rem
+                }
+
+                paragraph {
+                    style {
+                        text = "Other"
+                    }
                 }
             }
         }
@@ -127,7 +152,7 @@ val mainScreen = gui {
 }
 
 class ConfigGui: GuiScreen() {
-    var currentScreen: club.maxstats.kolour.gui.GuiScreen? = null
+    var currentScreen: GuiComponent? = null
 
     override fun drawScreen(
         mouseX: Int,
