3931f28727514e797df49fb2fcb666e838e1c14c
exejar <56853437+exejar@users.noreply.github.com>
2023-06-24 00:25:09 -0400

    Updated to the newest version of Kolour

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ runtime.isTransitive = false
 
 dependencies {
     runtime("com.github.exejar:HyKo:9c840f932e")
-    runtime("com.github.exejar:Kolour:ef10cd4b5a")
+    runtime("com.github.exejar:Kolour:e377afe8ae")
     runtime("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
     compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
     compileOnly("com.github.weave-mc:weave-loader:70bd82faa6")
